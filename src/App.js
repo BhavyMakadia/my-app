0da6f0533967e9e727d3f79b585a93cfa166530b
bhavytimepass <83721689+bhavytimepass@users.noreply.github.com>
2023-06-15 01:28:01 +0530

    bhavy20video

--- src/App.js
+++ src/App.js
@@ -1,58 +1,77 @@
-
 import './App.css';
-import About from './components/About';
 import Navbar from './components/Navbar';
-import Textforms from './components/Textforms';
+import TextForm from './components/Textforms';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,Link
+  Route
 } from "react-router-dom";
 
+ 
 function App() {
- //    const [alert, setAlert] = useState(null);
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+const removeBodyClasses=()=>{
+  document.body.classList.remove('bg-light')
+  document.body.classList.remove('bg-dark')
+  document.body.classList.remove('bg-warning')
+  document.body.classList.remove('bg-danger')
+  document.body.classList.remove('bg-success')
+  document.body.classList.remove('bg-primary')
   
-  /*  const showAlert = (message, type)=>{
-        setAlert({
-          msg: message,
-          type: type
-        })
-        setTimeout(() => {
-            setAlert(null);
-        }, 1500);
-    }*/
-/*const 
-    const toggleMode = ()=>{
-      if(mode === 'light'){
+}
+  const toggleMode = (cls)=>{
+    
+     /*  if(mode === 'light'){
         setMode('dark');
         document.body.style.backgroundColor = '#042743';
-       // showAlert("Dark mode has been enabled", "success");
+        showAlert("Dark mode has been enabled", "success");
       }
+      
       else{
         setMode('light');
         document.body.style.backgroundColor = 'white';
-        //showAlert("Light mode has been enabled", "success");
+        showAlert("Light mode has been enabled", "success");
       }
-    }*/
+    */
+    
+    removeBodyClasses();
+    console.log(cls);
+    document.body.classList.add('bg-'+cls)
+   
+  }
   return (
-
+    <>
     <Router>
-
-<Navbar /*title="bhavytextutils" mode={mode} toggleMode={toggleMode}*/ aboutText="About Textutils" />
-<div className="container my-4"  >
-        <Switch>
-              <Route exact path="/about">
-                <About />
-                </Route>
-
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} About="about" />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Switch>
+    {/* /users --> Component 1
+        /users/home --> Component 2 */}
+          <Route exact path="/about">
+            <About mode={mode} />
+          </Route>
           <Route exact path="/">
-              <Textforms heading="Try TextUtils - word counter, character counter, remove extra spaces" title="Enter a data in form of text:-"/>
+            <TextForm showAlert={showAlert}  title="Enter a data :-" mode={mode}/>
           </Route>
     </Switch>
-</div>    
+    </div>
     </Router>
-    
-
+    </> 
   );
 }
 
--- src/components/Textforms.js
+++ src/components/Textforms.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 
 export default function Textforms(props) 
 {
-    const[text,setText]=useState('');
+  
     //setText('ehfjksddh');
-
+    const [text, setText] = useState('');
     const handleupclick=()=>
     {
         console.log("on click");
@@ -46,18 +46,21 @@ const handleExtraSpaces = () => {
 }
  
     return(
-    <div style={{color: props.mode==='dark'?'white':'#042743'}}>
+    <div  className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
       
 
   <label for="myBox" className="form-label">{props.title}-{text}</label>
   <div className="mb-3">
-  <textarea className="form-control" value={text}  onChange={handleonchange} id="myBox" rows="6"/>
+  <textarea className="form-control" value={text}  onChange={handleonchange}  style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="6"/>
 </div>
 <button disabled={text.length===0} className="btn btn-primary mx-3 my-3" onClick={handleupclick} >uppercase</button>
 <button disabled={text.length===0} className="btn btn-primary mx-3 my-3" onClick={handleupclick1} >LowerCase</button>
         
-
-<div className="container my-3">
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+       
+<div className="container my-3"  style={{color: props.mode==='dark'?'white':'#042743'}}>
 <h1> some information</h1>
 <p> { 
 
